42fc1a81db5ce30c88802c1c8d74af25c8b143b3
P Gowtham Kumar Reddy <pgowthamreddy3@gmail.com>
2026-06-02 18:17:46 +0530

    Create DAY03_if-else staements.c

---
+++ DAY03_if-else staements.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main(){
+
+printf("enter the value : ");
+int b;
+scanf("%d",&b);
+if (b>5)
+ 
+      { printf("b=%d",b);
+       printf(" and the value is greater than 5");}       
+else
+    printf("given value is less than 5");     
+
+return 0;
+  }
