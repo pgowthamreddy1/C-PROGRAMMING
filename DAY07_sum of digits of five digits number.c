67f95b0924884b6e16ad660a7c26e60dabccb5d3
P Gowtham Kumar Reddy <pgowthamreddy3@gmail.com>
2026-06-11 17:17:45 +0530

    Add program to sum digits of a five-digit number

---
+++ DAY07_sum of digits of five digits number.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <conio.h>
+int main(){
+int a;
+
+printf("enter a five digit number: ");
+scanf("%d",&a);
+
+int b=a%10;
+int c=a/10;
+int d=c%10;
+int e=c/10;
+int f=e%10;
+int g=e/10;  
+int h=g%10;
+int j=g/10;
+int sum=b+d+f+h+j;
+printf("the sum of digits of the given number is : %d",sum);
+return 0;
+}
