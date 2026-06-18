ce4adb7259da3f578e1a76c13583288f87bcf1f1
P Gowtham Kumar Reddy <pgowthamreddy3@gmail.com>
2026-06-18 11:30:21 +0530

    Implement multiplication table generator

---
+++ DAY08_Tables.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+int a,j=1,k;
+printf("enter the numbers table you need to print: ");
+scanf("%d",&a);
+for(a,j,k=a*j;a,j<11;j++,k=a*j)
+  {
+    printf("%d*%d=%d",a,j,k);
+  }
+return 0;
+}
